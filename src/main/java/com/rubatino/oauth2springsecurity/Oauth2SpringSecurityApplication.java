2b9187eed0076b67034f5f8da832c6e6a27b760f
Vitor Rubatino <rubatinovitor@gmail.com>
2023-03-31 01:44:42 -0300

    initial oauth

---
+++ src/main/java/com/rubatino/oauth2springsecurity/Oauth2SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.rubatino.oauth2springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2SpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rubatino/oauth2springsecurity/Oauth2SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubatino.oauth2springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
